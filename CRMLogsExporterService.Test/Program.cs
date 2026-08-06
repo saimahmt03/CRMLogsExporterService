93395f0604327ffb46060a74b1dd80ad8ddb0a78
Saimah Tocalo <saimahtocalo@gmail.com>
2026-08-05 18:35:40 -0700

    Adding Console App for testing environment. We test the CRM Connectivity with the Windows Service by calling the configuration method implemented in Windows Service in the Console App since the Windows Service does not support Console.WriteLine which we need to print responses and to debug

---
+++ CRMLogsExporterService.Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using CRMLogsExporterService.Configuration;
+using CRMLogsExporterService.Services;
+using Microsoft.Crm.Sdk.Messages;
+using CRMLogsExporterService.Configuration;
+using CRMLogsExporterService.Services;
+
+namespace CRMLogsExporterService.Test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                var crmSettings = new CrmSettings
+                {
+                    Url = ConfigurationManager.AppSettings["CrmUrl"],
+                    Domain = ConfigurationManager.AppSettings["CrmDomain"],
+                    Username = ConfigurationManager.AppSettings["CrmUsername"],
+                    Password = ConfigurationManager.AppSettings["CrmPassword"]
+                };
+
+                Console.WriteLine("CRM URL:");
+                Console.WriteLine(crmSettings.Url);
+                Console.WriteLine();
+
+                var crmService = new CrmService(crmSettings);
+
+                var service = crmService.Connect();
+
+                var response = (WhoAmIResponse)service.Execute(new WhoAmIRequest());
+
+                Console.WriteLine("=================================");
+                Console.WriteLine("Connected to Dynamics CRM!");
+                Console.WriteLine($"User ID: {response.UserId}");
+                Console.WriteLine("=================================");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection Failed");
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
